36690c4c15060c912a3a0a3c7cd5fb339a68fcf9
udontur <h@hadrian.cc>
2025-12-01 09:29:52 +0800

    feat: init template

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int long long
+#define ff first
+#define ss second
+using namespace std;
+void udontur(){
+
+}
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tt; cin>>tt; while(tt--)
+    udontur();
+    return 0;
+}
